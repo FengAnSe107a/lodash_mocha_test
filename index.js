9e7030e85519d30dd930bf3f16a983290f2a864e
Jack Wang <sjkry505@gmail.com>
2018-10-12 10:56:53 +0800

    add new function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /** @namespace _ */
 module.exports = {
+    tail: require('./lib/tail'),
+    join: require('./lib/join'),
+    reverse: require('./lib/reverse'),
     difference: require('./lib/difference'),
     drop: require('./lib/drop'),
     flattenDeep: require('./lib/flattendeep'),
